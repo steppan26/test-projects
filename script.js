37e27f8a1175373613edab4b9a9c6a3ff213223c
Stephane Baroux <stephane.baroux@gmail.com>
2021-09-22 16:23:36 +0200

    installed react-app and react-spring

--- script.js
+++ script.js
@@ -1,34 +1,105 @@
-const ball = document.querySelector('[data-ball]')
+let dragHandles = [...document.getElementsByClassName('drag-handle')]
 const gooeyWrapper = document.querySelector('[data-gooey-wrapper]')
 const dropbox = document.querySelector('[data-dropbox]')
 const icon = document.querySelector('[data-icon]')
+const ball = document.querySelector('[data-ball]')
 
-gooeyWrapper.addEventListener('dragover', e => {
-    e.preventDefault()
-    let x = e.pageX;
-    let y = e.pageY;
 
-    y = e.pageY
-    x = e.pageX
+let selectedBall
 
-    ball.style.left = x + 'px';
-    ball.style.top = y + 'px';
+const colorsArray = ['blue', 'red']
+
+
+
+function setTestIconLocation(){
+    const ballPosition = ball.getBoundingClientRect()
+}
+
+ball.addEventListener('click', e => {
+    ball.style.bottom = "250px"
 })
 
-ball.addEventListener('dragstart', e => {
-})
 
-ball.addEventListener('dragend', e => {
-    const element = e.target
-    const containerTop = dropbox.offsetTop
-    const containerLeft = dropbox.offsetLeft
-    const containerBottom = dropbox.offsetTop + dropbox.offsetHeight
-    const containerRight = dropbox.offsetLeft + dropbox.offsetWidth
 
-    const elementX = element.offsetTop + (element.offsetHeight / 2)
-    const elementY = element.offsetLeft + (element.offsetWidth / 2)
 
-    if((elementY > containerTop && elementY < containerBottom) && (elementX > containerLeft && elementX < containerRight)){
-        dropbox.style.backgroundColor = "red"
-    }
-})
+
+// dragHandles.forEach((handle, index) => {
+//     handle.addEventListener('mousedown', mouseDownHandler)
+//     let ballPosition
+//     const dropboxPosition = getCoords(dropbox)
+
+//     handle.addEventListener('dragstart', e => {
+//         selectedBall = handle.parentNode
+//         ballPosition = getCoords(selectedBall)
+//     })
+
+//     handle.addEventListener('dragend', e => {
+//         e.preventDefault()
+//         ballPosition = getCoords(selectedBall)
+
+//         const containerTop = dropboxPosition.top
+//         const containerLeft = dropboxPosition.left
+//         const containerBottom = dropboxPosition.top + dropbox.offsetHeight
+//         const containerRight = dropboxPosition.left + dropbox.offsetWidth
+
+//         const elementY = ballPosition.top + (selectedBall.offsetHeight / 2)
+//         const elementX = ballPosition.left + (selectedBall.offsetWidth / 2)
+//         if((elementY > containerTop && elementY < containerBottom) && (elementX > containerLeft && elementX < containerRight)){
+//             dropbox.style.backgroundColor = colorsArray[index]
+//             console.log(true, elementX, elementY)
+//         } else {
+//             console.log(false,"element ", elementX, elementY, "container ", containerTop, containerLeft, containerBottom, containerRight)
+//         }
+//     })
+// })
+
+// document.addEventListener('drag', e => {
+//     console.log(e.movementY)
+// })
+
+// document.addEventListener('dragover', e => {
+//     e.preventDefault()
+//     ballPosition = getCoords(selectedBall)
+//     console.log(e.movementY)
+
+//     const elementY = ballPosition.top + 27
+//     const elementX = ballPosition.left + 27
+//     const elementBottom = getCoords(selectedBall.parentNode)
+
+//     selectedBall.style.left = elementX + 'px';
+//     selectedBall.style.bottom = elementBottom.top + 'px';
+
+
+// })
+
+// function getCoords(elem) {
+//     let box = elem.getBoundingClientRect();
+
+//     let body = document.body;
+//     let docEl = document.documentElement;
+
+//     let scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+//     let scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+//     let clientTop = docEl.clientTop || body.clientTop || 0;
+//     let clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+//     let top  = box.top +  scrollTop - clientTop;
+//     let left = box.left + scrollLeft - clientLeft;
+
+//     return { top: Math.round(top), left: Math.round(left) };
+// }
+
+// function getPositionOffset(elem) {
+//     const elemPosition = getCoords(elem)
+//     let top
+//     let bottom
+//     let containerPosition = getCoords(elem.parentNode)
+
+//     if(elemPosition.top < containerPosition.top){
+//         top = elemPosition.top - containerPosition.top
+//     } else {
+//         top = elemPosition.top - containerPosition.top
+//     }
+//     return { top: Math.round(top), bottom: Math.round(bottom) }
+// }
